bf4e4646ed3829e777cd3758e52dbf33323c83c2
james <james.speirs@gmail.com>
2019-01-15 14:49:40 -0700

    write tests

--- test/index.test.js
+++ test/index.test.js
@@ -15,56 +15,164 @@
  *    limitations under the License.
  **/
 'use strict'
-const expect = require('chai').expect
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+const Enforcer = require('../index')
+const helper = require('./resources/helper')
+const path = require('path')
+
+const expect = chai.expect
+chai.use(chaiAsPromised)
 
 /* global describe it */
 describe('openapi-enforcer-middleware', () => {
   describe('map controllers', () => {
-    describe('missing controller files', () => {
-      it('warns in development', () => {
-        throw Error('TODO')
-      })
-
-      it('throws error in production', () => {
-        throw Error('TODO')
-      })
+    it('will produce exception for invalid controller target', () => {
+      const definition = helper.definition.v3()
+      definition['x-controller'] = 'dne'
+      definition.paths['/'].get['x-operation'] = 'dne'
+      const enforcer = Enforcer(definition)
+      const promise = enforcer.controllers()
+      return expect(promise).to.be.rejectedWith(/Controllers target must be a string, a non-null object, or a function that returns a non-null object/)
     })
 
-    describe('controller operations', () => {
-      it('warns in development if missing', () => {
-        throw Error('TODO')
+    it('will produce exception for missing controller file', () => {
+      const definition = helper.definition.v3()
+      definition['x-controller'] = 'dne'
+      definition.paths['/'].get['x-operation'] = 'dne'
+      const enforcer = Enforcer(definition)
+      const promise = enforcer.controllers(path.resolve(__dirname, 'resources'))
+      return expect(promise).to.be.rejectedWith(/at: dne\s+Controller file not found/)
+    })
+
+    it('will produce exception for missing operation', () => {
+      const definition = helper.definition.v3()
+      definition['x-controller'] = 'controller'
+      definition.paths['/'].get['x-operation'] = 'dne'
+      const enforcer = Enforcer(definition)
+      const promise = enforcer.controllers(path.resolve(__dirname, 'resources'))
+      return expect(promise).to.be.rejectedWith(/at: controller\s+Operation not found: dne/)
+    })
+
+    it('will produce exception for missing mapped controller', () => {
+      const definition = helper.definition.v3()
+      definition['x-controller'] = 'dne'
+      definition.paths['/'].get['x-operation'] = 'dne'
+      const enforcer = Enforcer(definition)
+      const promise = enforcer.controllers({})
+      return expect(promise).to.be.rejectedWith(/at: dne\s+Controller not found/)
+    })
+
+    it('will produce exception for missing operation of mapped controller', () => {
+      const definition = helper.definition.v3()
+      definition['x-controller'] = 'controller'
+      definition.paths['/'].get['x-operation'] = 'dne'
+      const enforcer = Enforcer(definition)
+      const promise = enforcer.controllers({ controller: {} })
+      return expect(promise).to.be.rejectedWith(/at: controller\s+Controller operation not found: dne/)
+    })
+
+    it('will allow an operation to be a function', () => {
+      const definition = helper.definition.v3()
+      definition['x-controller'] = 'controller'
+      definition.paths['/'].get['x-operation'] = 'empty'
+      const enforcer = Enforcer(definition)
+      const promise = enforcer.controllers({ controller: { empty: () => {} } })
+      return expect(promise).to.eventually.be.a('Map')
+    })
+
+    it('will allow an operation to be an array of functions', () => {
+      const definition = helper.definition.v3()
+      definition['x-controller'] = 'controller'
+      definition.paths['/'].get['x-operation'] = 'empty'
+      const enforcer = Enforcer(definition)
+      const promise = enforcer.controllers({
+        controller: {
+          empty: [() => {}]
+        }
+      })
+      return expect(promise).to.eventually.be.a('Map')
+    })
+
+    it('will produce an exception if an operation is an array not of functions', () => {
+      const definition = helper.definition.v3()
+      definition['x-controller'] = 'controller'
+      definition.paths['/'].get['x-operation'] = 'invalid'
+      const enforcer = Enforcer(definition)
+      const promise = enforcer.controllers({
+        controller: {
+          invalid: [1]
+        }
+      })
+      return expect(promise).to.be.rejectedWith(/at: 0\s+Not a function/)
+    })
+
+    it('will produce an exception if an operation is not a function', () => {
+      const definition = helper.definition.v3()
+      definition['x-controller'] = 'controller'
+      definition.paths['/'].get['x-operation'] = 'invalid'
+      const enforcer = Enforcer(definition)
+      const promise = enforcer.controllers({
+        controller: {
+          invalid: 1
+        }
+      })
+      return expect(promise).to.be.rejectedWith(/at: controller\s+Expected a function or an array of functions. Received: 1/)
+    })
+
+    it('allows the controllers target to be a function', () => {
+      const definition = helper.definition.v3()
+      definition['x-controller'] = 'controller'
+      definition.paths['/'].get['x-operation'] = 'empty'
+      const enforcer = Enforcer(definition)
+      const promise = enforcer.controllers(function () {
+        return {
+          controller: {
+            empty: [() => {}]
+          }
+        }
+      })
+      return expect(promise).to.eventually.be.a('Map')
+    })
+
+    it('throw an exception if the controllers target as a function does not return a non-null object', () => {
+      const definition = helper.definition.v3()
+      definition['x-controller'] = 'controller'
+      definition.paths['/'].get['x-operation'] = 'empty'
+      const enforcer = Enforcer(definition)
+      const promise = enforcer.controllers(function () {
+        return null
+      })
+      return expect(promise).to.be.rejectedWith(/Controllers target function must return a non-null object/)
+    })
+
+    describe('dependency injection', () => {
+      it('injects for controllers target as function', async () => {
+        const injected = []
+        function target (a, b, c) {
+          injected.push(a, b, c)
+          return {}
+        }
+
+        const enforcer = Enforcer(helper.definition.v3())
+        await enforcer.controllers(target, 1, 2, 3)
+        expect(injected).to.deep.equal([1, 2, 3])
       })
 
-      it('throws error in production if missing', () => {
-        throw Error('TODO')
-      })
+      it('injects for controllers target as file path when controller file returns function', async () => {
+        const definition = helper.definition.v3()
+        definition['x-controller'] = 'injectable'
+        definition.paths['/'].get['x-operation'] = 'injected'
+        const enforcer = Enforcer(definition)
+        enforcer.controllers(path.resolve(__dirname, 'resources'), 1, 'b', false)
 
-      it('can be a function', () => {
-        throw Error('TODO')
-      })
-
-      it('can be an array of functions', () => {
-        throw Error('TODO')
-      })
-
-      it('can be an empty array', () => {
-        throw Error('TODO')
-      })
-
-      it('cannot be an array with item not as function', () => {
-        throw Error('TODO')
-      })
-
-      it('cannot be an object', () => {
-        throw Error('TODO')
+        const res = await helper.request(enforcer)
+        expect(res.body).to.deep.equal([1, 'b', false])
       })
     })
   })
 
-  describe('in memory controllers', () => {
-    it('allows a controller map to be specified', () => {
-      // this refers to not specifying a controller file path but rather an object containing the already built controller map
-      throw Error('TODO')
-    })
+  describe('run controllers', () => {
+
   })
 })
