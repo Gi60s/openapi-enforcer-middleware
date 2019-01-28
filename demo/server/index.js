b99b84d37295a9f52a31ed49859993063afdd1d9
james <james.speirs@gmail.com>
2019-01-28 15:40:47 -0700

    db only created if env vars exist

--- demo/server/index.js
+++ demo/server/index.js
@@ -1,19 +1,3 @@
-/**
- *  @license
- *    Copyright 2018 Brigham Young University
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- **/
 'use strict'
 const { Pool } = require('pg')
 const express = require('express')
@@ -26,13 +10,6 @@ app.use((req, res, next) => {
   j(req, res, next)
 })
 
-const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD
-})
-
 const enforcer = Enforcer(path.resolve(__dirname, 'openapi-v2.yaml'))
 
 // this middleware will handle explicit mock requests
@@ -40,8 +17,17 @@ enforcer.mocks(path.resolve(__dirname, 'mock-controllers'), false)
   .catch(console.error)
 
 // this middleware will handle non-mocked requests
-enforcer.controllers(path.resolve(__dirname, 'controllers'), pool)
-  .catch(console.error)
+if (process.env.POSTGRES_HOST) {
+  const pool = new Pool({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD
+  })
+
+  enforcer.controllers(path.resolve(__dirname, 'controllers'), pool)
+    .catch(console.error)
+}
 
 // this middleware will automatically run fallback mocks
 enforcer.mocks(path.resolve(__dirname, 'mock-controllers'), true)
