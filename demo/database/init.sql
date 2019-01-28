86e411c14fa44c89b76cc7acd6c252b28215a4cc
james <james.speirs@gmail.com>
2019-01-28 14:47:53 -0700

    get database working with initial data

--- demo/database/init.sql
+++ demo/database/init.sql
@@ -3,8 +3,10 @@
 
 -- This table keeps track of all valid events that have occurred
 CREATE TABLE IF NOT EXISTS tasks (
- index SERIAL PRIMARY KEY,
+ id SERIAL PRIMARY KEY,
  title VARCHAR (256) NOT NULL,
  completed BOOLEAN DEFAULT FALSE,
- due TIMESTAMPZ NOT NULL
+ due TIMESTAMPTZ NOT NULL
 );
+
+INSERT INTO tasks (title, completed, due) VALUES ('My First Task', FALSE, '2020-01-02 04:05:06 +0:00');
