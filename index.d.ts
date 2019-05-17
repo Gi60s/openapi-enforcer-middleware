0fc76fd7603374002c2c7ded51d0935ea9a74b4e
james <james.speirs@gmail.com>
2019-05-17 14:44:01 -0600

    add componentOptions to ts def

--- index.d.ts
+++ index.d.ts
@@ -24,6 +24,7 @@ declare namespace OpenApiEnforcerMiddleware {
     }
 
     export interface Options {
+        componentOptions?: object;
         fallThrough?: boolean;
         mockHeader?: string;
         mockQuery?: string;
