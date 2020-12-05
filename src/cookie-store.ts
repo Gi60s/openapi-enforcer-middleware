f263cfd4caeef5b4155e7fdad89b4ee64863092b
James Speirs <james.speirs@gmail.com>
2020-12-05 10:34:17 -0700

    use === comparator

--- src/cookie-store.ts
+++ src/cookie-store.ts
@@ -48,7 +48,7 @@ function getCookie(headers: IncomingHttpHeaders): string {
     const ca = decodedCookie.split(';')
     for(let i = 0; i < ca.length; i++) {
         let c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1)
+        while (c.charAt(0) === ' ') c = c.substring(1)
         if (c.indexOf(name) === 0) return c.substring(name.length, c.length)
     }
     return ""
