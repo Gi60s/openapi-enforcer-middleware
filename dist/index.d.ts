40e8dfa79f761bd2617c0ba7e0a0445b6be2d8f2
James Speirs <james.speirs@gmail.com>
2021-01-25 10:43:19 -0700

    build def

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,9 +3,9 @@ import * as I from './interfaces';
 export = OpenAPIEnforcerMiddleware;
 declare function OpenAPIEnforcerMiddleware(enforcerPromise: Promise<any>): {
     init(options?: I.MiddlewareOptions | undefined): I.Middleware;
-    mock(): (req: import("express").Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs>, res: import("express").Response<any>, next: import("express").NextFunction) => void;
+    mock(): (req: import("express").Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>, res: import("express").Response<any, Record<string, any>>, next: import("express").NextFunction) => void;
     on: typeof on;
-    route(controllersDir: string, dependencies?: any[] | import("./route-builder").DependencyMap | undefined, options?: I.RouteBuilderOptions | undefined): (req: import("express").Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs>, res: import("express").Response<any>, next: import("express").NextFunction) => void;
+    route(controllersDir: string, dependencies?: any[] | import("./route-builder").DependencyMap | undefined, options?: I.RouteBuilderOptions | undefined): (req: import("express").Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>, res: import("express").Response<any, Record<string, any>>, next: import("express").NextFunction) => void;
 };
 declare namespace OpenAPIEnforcerMiddleware {
     var _a: typeof OpenAPIEnforcerMiddleware;
