d3dc911cd3562554c6befb9aedf1cbac43d116b2
James Speirs <james.speirs@gmail.com>
2021-10-07 15:24:01 -0600

    fix async enforcer problem

--- demo/v2/server.js
+++ demo/v2/server.js
@@ -2,31 +2,33 @@ const express = require('express')
 const Enforcer = require('openapi-enforcer')
 const EnforcerMiddleware = require('../../dist')
 const path = require('path')
+const users = require('./controllers/users')
 
-const openapiPath = path.resolve(__dirname, 'openapi.yml')
-const enforcer = EnforcerMiddleware(Enforcer(openapiPath))
+async function server () {
+  const openapiPath = path.resolve(__dirname, 'openapi.yml')
+  const enforcer = EnforcerMiddleware(await Enforcer(openapiPath))
 
-enforcer.on('error', err => {
-  console.error(err)
-})
+  enforcer.on('error', err => {
+    console.error(err)
+  })
 
-const app = express()
-app.use(express.json())
+  const app = express()
+  app.use(express.json())
 
-app.use('/api', enforcer.init())
+  app.use('/api', enforcer.init())
 
-// const controllersPath = path.resolve(__dirname, 'controllers')
-// app.use(enforcer.route(controllersPath))
+  app.use('/api', enforcer.route({
+    users: users()
+  }))
 
-app.use(enforcer.route({
-  users: require('./controllers/users')
-}))
+  app.use('/docs', enforcer.docs({
+    postRedocInitScripts: ['/foo.js', './bar.js']
+  }))
 
-app.use('/docs', enforcer.docs({
-  postRedocInitScripts: ['/foo.js', './bar.js']
-}))
+  const listener = app.listen(8000, err => {
+    if (err) return console.error(err.stack)
+    console.log('Listening on port ' + listener.address().port)
+  })
+}
 
-const listener = app.listen(8000, err => {
-  if (err) return console.error(err.stack)
-  console.log('Listening on port ' + listener.address().port)
-})
+server().catch(console.error)
